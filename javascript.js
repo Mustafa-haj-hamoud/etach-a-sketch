cee8f98f38a28abe080cf850fa79ff0115aa38c1
Mustafa-haj-hamoud <142098665+Mustafa-haj-hamoud@users.noreply.github.com>
2024-03-14 02:17:31 +0300

    Add function to create a full drawing grid

--- javascript.js
+++ javascript.js
@@ -1,12 +1,13 @@
 //get container width to calculate the side length of each square
-const containerWidth = document.querySelector("#container").getBoundingClientRect().width;
 const gridContainer = document.querySelector("#grid-container");
+const containerWidth = gridContainer.getBoundingClientRect().width;
+
 
 function createSquare(row,sideLength) {
     const square = document.createElement("div");
-    square.textContent = "12";
     square.classList.add("square");
-    square.style.minHeight=`${sideLength}px`;
+    square.style["min-width"] = `${sideLength}px`;
+    square.style["min-height"] = `${sideLength}px`;
     square.style["width"] = `${sideLength}px`;
     square.style["height"] = `${sideLength}px`;
     row.appendChild(square);
@@ -17,15 +18,17 @@ function createRow(squareCount){
     let squareSideLength = Number(containerWidth) / squareCount;
     const row = document.createElement("div");
     row.classList.add("row");
-    //implement function to create a square with specified dimension
-    //temp code below this line
     for (let i = 0; i < squareCount; i++){
         createSquare(row,squareSideLength);
     }
-    //temp code ends here
-    
     gridContainer.appendChild(row);
 }
 
-createRow(16);
+function createGrid(rowCount) {
+    for (let i = 0; i < rowCount; i++){
+        createRow(rowCount);
+    }
+}
+
+createGrid(16);
 //add event listeners to each square in the grid
